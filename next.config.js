3c2d50b7cea60a30de9a6ead469bc959a0512cd2
montesalberto-source <montesalberto@hotmail.com>
2026-08-07 17:32:21 -0600

    Clear build cache

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
+// cache-bust: 1786145540
 const nextConfig = {
-  reactStrictMode: true,
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  reactStrictMode: false,
+  swcMinify: true,
 }
 module.exports = nextConfig
